0681588ae370267dca88a73aad84f99253bf6632
Harran Ali <harran.m@gmail.com>
2021-05-17 09:27:28 +0200

    add assign permission test

--- authority_test.go
+++ authority_test.go
@@ -75,7 +75,7 @@ func TestCreatePermission(t *testing.T) {
 		DB:           db,
 	})
 
-	// test create role
+	// test create permission
 	err := auth.CreatePermission("permission-a")
 	if err != nil {
 		t.Error("an error was not expected while creating permision ", err)
@@ -91,14 +91,58 @@ func TestCreatePermission(t *testing.T) {
 	}
 
 	// test duplicated entries
-	auth.CreateRole("permission-a")
-	auth.CreateRole("permission-a")
-	auth.CreateRole("permission-a")
+	auth.CreatePermission("permission-a")
+	auth.CreatePermission("permission-a")
+	auth.CreatePermission("permission-a")
 	db.Model(authority.Role{}).Where("name = ?", "permission-a").Count(&c)
 	if c > 1 {
 		t.Error("unexpected duplicated entries for permission")
 	}
 
 	// clean up
-	db.Where("name = ?", "permission-a").Delete(authority.Role{})
+	db.Where("name = ?", "permission-a").Delete(authority.Permission{})
+}
+
+func TestAssignPermission(t *testing.T) {
+	auth := authority.New(authority.Options{
+		TablesPrefix: "authority_",
+		DB:           db,
+	})
+
+	// first create a role
+	err := auth.CreateRole("role-a")
+	if err != nil {
+		t.Error("unexpected error while creating role to be assigned.", err)
+	}
+
+	// second test create permissions
+	err = auth.CreatePermission("permission-a")
+	if err != nil {
+		t.Error("unexpected error while creating permission to be assigned.", err)
+	}
+	err = auth.CreatePermission("permission-b")
+	if err != nil {
+		t.Error("unexpected error while creating permission to be assigned.", err)
+	}
+
+	// assign the permissions
+	err = auth.AssignPermissions("role-a", []string{"permission-a", "permission-b"})
+	if err != nil {
+		t.Error("unexpected error while assigning permissions.", err)
+	}
+
+	// assert
+	var r authority.Role
+	db.Where("name = ?", "role-a").First(&r)
+	var rolePermsCount int64
+	db.Model(authority.RolePermission{}).Where("role_id = ?", r.ID).Count(&rolePermsCount)
+	if rolePermsCount != 2 {
+		t.Error("failed assigning roles to permission")
+	}
+
+	// clean up
+	db.Where("role_id = ?", r.ID).Delete(authority.RolePermission{})
+	db.Where("name = ?", "role-a").Delete(authority.Role{})
+	db.Where("name = ?", "permission-a").Delete(authority.Permission{})
+	db.Where("name = ?", "permission-b").Delete(authority.Permission{})
 }
