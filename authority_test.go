3926a77fdffc48644b915d85674b51a4ad4bd1a7
Harran Ali <harran.m@gmail.com>
2021-07-31 16:58:05 +0200

    update test database config

--- authority_test.go
+++ authority_test.go
@@ -23,8 +23,10 @@ func TestMain(m *testing.M) {
 	var dsn string
 	if os.Getenv("env") == "testing" {
 		fmt.Println("preparing testing config...")
-		dsn = fmt.Sprintf("root:%s@tcp(127.0.0.1:3306)/db_test?charset=utf8mb4&parseTime=True&loc=Local",
-			os.Getenv("ROOT_PASSWORD"))
+		dsn = fmt.Sprintf("root:@tcp(127.0.0.1:3306)/db_test?charset=utf8mb4&parseTime=True&loc=Local")
+
+		// dsn = fmt.Sprintf("root:%s@tcp(127.0.0.1:3306)/db_test?charset=utf8mb4&parseTime=True&loc=Local",
+		// 	os.Getenv("ROOT_PASSWORD"))
 	} else {
 		dsn = "root:@tcp(127.0.0.1:3306)/db_test?charset=utf8mb4&parseTime=True&loc=Local"
 	}
